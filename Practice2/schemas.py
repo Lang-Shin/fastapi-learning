b2db2492d31c1c9a797baa031dc8c27227316bd9
Charles <bajuracharles@gmail.com>
2026-07-26 10:05:18 +0800

    feat: schemas ready for database

--- Practice2/schemas.py
+++ Practice2/schemas.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel, ConfigDict, Field
 
 
+class ReviewBase(BaseModel):
+    reviewer: str = Field(min_length=1, max_length=200)
+    rating: int = Field(ge=1, le=5)
+    comment: str = Field(max_length=1000)
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewResponse(ReviewBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+
 class AuthorBase(BaseModel):
     name: str = Field(min_length=1, max_length=100)
     bio: str = Field(max_length=500)
-    initials: str = Field(max_length=500)
+    initials: str = Field(max_length=10)
     
 class AuthorCreate(AuthorBase):
     pass
@@ -16,7 +29,7 @@ class AuthorResponse(AuthorBase):
     
     
 class BookBase(BaseModel):
-    title: int = Field(min_length=1, max_length=100)
+    title: str = Field(min_length=1, max_length=200)
     genre: str = Field(min_length=1, max_length=200)
     pages: int
     year: int
@@ -24,10 +37,9 @@ class BookBase(BaseModel):
     reviews: list[dict]
     
 class BookCreate(BookBase):
-    pass
+    author_id: int
 
 class BookResponse(BookBase):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
-    authorId: int
